f287f3556edc2889741f258fb65ee2a003936732
axianibiru <axianibiru@163.com>
2023-04-26 17:56:30 +0800

    add guessing game project

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+D:\dev\Clion\Rust\my_rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+D:\dev\Clion\Rust\my_rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+D:\dev\Clion\Rust\my_rust\guessing_game\target\debug\guessing_game.exe: D:\dev\Clion\Rust\my_rust\guessing_game\src\main.rs
