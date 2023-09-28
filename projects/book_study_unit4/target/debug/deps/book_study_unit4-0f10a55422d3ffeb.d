d1f49c8290a9f5c6d4c071396e2a2719a4c1b17d
axianibiru <axianibiru@163.com>
2023-09-28 15:39:36 +0800

    初始化第四单元学习项目

---
+++ projects/book_study_unit4/target/debug/deps/book_study_unit4-0f10a55422d3ffeb.d
@@ -0,0 +1,5 @@
+D:\dev\RustRover\my_rust\projects\book_study_unit4\target\debug\deps\book_study_unit4-0f10a55422d3ffeb.rmeta: src\main.rs
+
+D:\dev\RustRover\my_rust\projects\book_study_unit4\target\debug\deps\book_study_unit4-0f10a55422d3ffeb.d: src\main.rs
+
+src\main.rs:
